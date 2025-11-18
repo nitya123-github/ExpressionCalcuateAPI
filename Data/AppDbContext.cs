935dcd701bed4d5f1259c25761196d6e87b5a5a3
Nitya <paladugunitya@gmail.com>
2025-11-18 17:48:00 +0000

    Fix: restore controller, add EF migration, apply migrations at startup, enable Swagger root redirect

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,14 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using ExpressionCalculatorAPI.Models;
 
-
 namespace ExpressionCalculatorAPI.Data
 {
-public class AppDbContext : DbContext
-{
-public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-
-public DbSet<ExpressionHistory> ExpressionHistories { get; set; }
-}
+		public DbSet<ExpressionHistory> ExpressionHistories { get; set; }
+	}
 }
---
+++ Migrations/20251118174244_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpressionCalculatorAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExpressionHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Expression = table.Column<string>(type: "TEXT", nullable: false),
+                    Result = table.Column<decimal>(type: "TEXT", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpressionHistories", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExpressionHistories");
+        }
+    }
+}
