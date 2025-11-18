fd96da266e7e2f2a9875789269a7bbb64ae18810
Nitya <paladugunitya@gmail.com>
2025-11-18 20:54:53 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using ExpressionCalculatorAPI.Data;
 
-namespace dotnetcore
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+var app = builder.Build();
+
+
+// Auto-migrate DB on startup
+using (var scope = app.Services.CreateScope())
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+db.Database.Migrate();
 }
+
+
+if (app.Environment.IsDevelopment())
+{
+app.UseSwagger();
+app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
